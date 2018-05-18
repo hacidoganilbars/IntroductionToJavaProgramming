a3677de181c578548545d56709733a177ff6b710
hdilbars <hd.ilbars@hotmail.com>
2018-05-18 21:38:30 +0300

    introduction java programming 10th

---
+++ JavaCalisma/src/tr/bolum03/MathRandomOrnek.java
@@ -0,0 +1,21 @@
+package tr.bolum03;
+
+public class MathRandomOrnek {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		
+	}
+
+}
