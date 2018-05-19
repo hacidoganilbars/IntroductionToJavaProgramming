975e0a2511dd336da58ff0c10c08621e327cefaa
hdilbars <hd.ilbars@hotmail.com>
2018-05-20 01:01:40 +0300

    obeb (ortak bolenlerin en buyugu) while

---
+++ JavaCalisma/src/tr/bolum05/OBEB.java
@@ -0,0 +1,26 @@
+package tr.bolum05;
+
+import java.util.Scanner;
+
+public class OBEB {
+
+	public static void main(String[] args) {
+		Scanner klavye = new Scanner(System.in);
+		System.out.println("1.Sayýyý giriniz: ");
+		int sayi1 = klavye.nextInt();
+		System.out.println("2.Sayýyý giriniz: ");
+		int sayi2 = klavye.nextInt();
+
+		int obeb = 1;
+		int bolen = 2;
+		while (bolen <= sayi1 && bolen <= sayi2) {
+			if (sayi1 % bolen == 0 && sayi2 % bolen == 0) {
+				obeb = bolen;
+			}
+			bolen++;
+		}
+		System.out.println(sayi1 + " ve " + sayi2 + " sayilarinin obebi: " + obeb);
+
+	}
+
+}
