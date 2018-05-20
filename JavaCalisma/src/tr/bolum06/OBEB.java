39283847f16d4ad4919d2df1a999464dbb47e325
hdilbars <hd.ilbars@hotmail.com>
2018-05-20 23:54:47 +0300

    method

---
+++ JavaCalisma/src/tr/bolum06/OBEB.java
@@ -0,0 +1,30 @@
+package tr.bolum06;
+
+import java.util.Scanner;
+
+public class OBEB {
+
+	public static void main(String[] args) {
+		Scanner klavye = new Scanner(System.in);
+		System.out.println("1.Sayýyý giriniz: ");
+		int sayi1 = klavye.nextInt();
+		System.out.println("2.Sayýyý giriniz: ");
+		int sayi2 = klavye.nextInt();
+
+		int obeb = obeb(sayi1, sayi2);
+		System.out.println(sayi1 + " ve " + sayi2 + " sayilarinin obebi: " + obeb);
+
+	}
+
+	private static int obeb(int sayi1, int sayi2) {
+		int obeb = 1;
+		int bolen = 2;
+		while (bolen <= sayi1 && bolen <= sayi2) {
+			if (sayi1 % bolen == 0 && sayi2 % bolen == 0) {
+				obeb = bolen;
+			}
+			bolen++;
+		}
+		return obeb;
+	}
+}
