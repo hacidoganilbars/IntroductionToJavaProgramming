bbaa95140e46c7c086a4433c74a8b5b2e3fcd1e0
hdilbars <hd.ilbars@hotmail.com>
2018-05-20 13:59:58 +0300

    asal sayi

---
+++ JavaCalisma/src/tr/bolum05/AsalSayi.java
@@ -0,0 +1,26 @@
+package tr.bolum05;
+
+public class AsalSayi {
+	public static void main(String[] args) {
+		
+		int asalSayiSayaci=0;
+		int sayi=2;
+		
+		while(asalSayiSayaci<50) {
+			boolean asalMi=true;
+			for (int i = 2; i < sayi; i++) {
+				if(sayi%i==0) {
+					asalMi=false;
+				}
+				
+			}
+			if(asalMi==true) {
+				asalSayiSayaci++;
+				System.out.println(asalSayiSayaci+". Asal Sayý: "+sayi);
+			}
+			sayi++;
+		}
+		
+	}
+
+}
