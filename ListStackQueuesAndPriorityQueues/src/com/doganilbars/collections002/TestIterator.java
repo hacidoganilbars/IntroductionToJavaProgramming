fa4d0896d9779df55e405f1bdc9ee54977455800
hdilbars <hd.ilbars@hotmail.com>
2018-06-26 04:22:21 +0300

    LISTS, STACKS, QUEUES, AND PRIORITY QUEUES

--- ListStackQueuesAndPriorityQueues/src/com/doganilbars/collections002/TestIterator.java
+++ ListStackQueuesAndPriorityQueues/src/com/doganilbars/collections002/TestIterator.java
@@ -6,21 +6,22 @@ import java.util.Iterator;
 
 public class TestIterator {
 	public static void main(String[] args) {
-		Collection<String> collection=new ArrayList<>();
+		Collection<String> collection = new ArrayList<>();
 		collection.add("Adana");
 		collection.add("Osmaniye");
 		collection.add("Ýstanbul");
 		collection.add("Balýkesir");
 		collection.add("Ýzmir");
 		collection.add("Manisa");
-		
+
 		for (String string : collection) {
-			System.out.print(string.toUpperCase()+" ");
-		}System.out.println();
-		
-		Iterator<String> iterator=collection.iterator();
+			System.out.print(string.toUpperCase() + " ");
+		}
+		System.out.println();
+
+		Iterator<String> iterator = collection.iterator();
 		while (iterator.hasNext()) {
-			System.out.print(iterator.next().toUpperCase()+" ");
+			System.out.print(iterator.next().toUpperCase() + " ");
 		}
 		System.out.println();
 	}
